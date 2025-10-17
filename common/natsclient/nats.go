011fee52e7bc3625ea7317ff11964080dd76c13c
Jim Marino <jim.marino@gmail.com>
2025-10-17 07:48:33 +0200

    Consolidate files

--- common/natsclient/nats.go
+++ common/natsclient/nats.go
@@ -10,6 +10,8 @@
 //       Metaform Systems, Inc. - initial API and implementation
 //
 
+//go:generate mockery
+
 package natsclient
 
 import (
@@ -73,4 +75,38 @@ func (nc *NatsClient) Close() {
 	if nc.Connection != nil {
 		nc.Connection.Close()
 	}
+}
+
+// MsgClient is an interface for interacting with NATS. This interface is used to allow for mocking in unit tests that
+// verify correct behavior in response to error conditions (i.e., negative tests).
+type MsgClient interface {
+	Update(ctx context.Context, key string, value []byte, version uint64) (uint64, error)
+	Stream(ctx context.Context, streamName string) (jetstream.Stream, error)
+	Get(ctx context.Context, key string) (jetstream.KeyValueEntry, error)
+	Publish(ctx context.Context, subject string, payload []byte, opts ...jetstream.PublishOpt) (*jetstream.PubAck, error)
+}
+
+func NewMsgClient(nc *NatsClient) MsgClient {
+	return natsClientAdapter{Client: nc}
+}
+
+// Wraps the NatsClient to satisfy the MsgClient interface.
+type natsClientAdapter struct {
+	Client *NatsClient
+}
+
+func (a natsClientAdapter) Update(ctx context.Context, key string, value []byte, version uint64) (uint64, error) {
+	return a.Client.KVStore.Update(ctx, key, value, version)
+}
+
+func (a natsClientAdapter) Stream(ctx context.Context, streamName string) (jetstream.Stream, error) {
+	return a.Client.JetStream.Stream(ctx, streamName)
+}
+
+func (a natsClientAdapter) Get(ctx context.Context, key string) (jetstream.KeyValueEntry, error) {
+	return a.Client.KVStore.Get(ctx, key)
+}
+
+func (a natsClientAdapter) Publish(ctx context.Context, subject string, payload []byte, opts ...jetstream.PublishOpt) (*jetstream.PubAck, error) {
+	return a.Client.JetStream.Publish(ctx, subject, payload, opts...)
 }
