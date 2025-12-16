5e41cd5a97f608356d492e0805b913df9541386d
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2025-12-16 12:11:45 +0100

    feat: add RegistrationAgent

---
+++ agent/registration/activity/activity.go
@@ -0,0 +1,63 @@
+//  Copyright (c) 2025 Metaform Systems, Inc
+//
+//  This program and the accompanying materials are made available under the
+//  terms of the Apache License, Version 2.0 which is available at
+//  https://www.apache.org/licenses/LICENSE-2.0
+//
+//  SPDX-License-Identifier: Apache-2.0
+//
+//  Contributors:
+//       Metaform Systems, Inc. - initial API and implementation
+//
+
+package activity
+
+import (
+	"fmt"
+
+	"github.com/metaform/connector-fabric-manager/agent/registration/issuerservice"
+	"github.com/metaform/connector-fabric-manager/common/system"
+	"github.com/metaform/connector-fabric-manager/pmanager/api"
+)
+
+type RegistrationActivityProcessor struct {
+	Monitor       system.LogMonitor
+	IssuerService issuerservice.ApiClient
+}
+
+type RegistrationData struct {
+	DID        string `json:"cfm.participant.id" validate:"required"`
+	HolderName string `json:"cfm.participant.holdername"`
+}
+
+func NewProcessor(config *Config) *RegistrationActivityProcessor {
+	return &RegistrationActivityProcessor{
+		Monitor:       config.LogMonitor,
+		IssuerService: config.IssuerService,
+	}
+}
+
+type Config struct {
+	system.LogMonitor
+	IssuerService issuerservice.ApiClient
+}
+
+func (p RegistrationActivityProcessor) Process(ctx api.ActivityContext) api.ActivityResult {
+	var registrationData RegistrationData
+	if err := ctx.ReadValues(&registrationData); err != nil {
+		return api.ActivityResult{Result: api.ActivityResultFatalError, Error: fmt.Errorf("error processing Registration activity for orchestration %s: %w", ctx.OID(), err)}
+	}
+
+	if registrationData.HolderName == "" {
+		registrationData.HolderName = registrationData.DID
+	}
+
+	if err := p.IssuerService.CreateHolder(registrationData.DID, registrationData.DID, registrationData.HolderName); err != nil {
+		// todo: inspect error if it is retryable
+		return api.ActivityResult{Result: api.ActivityResultFatalError, Error: fmt.Errorf("error creating holder in ApiClient: %w", err)}
+	}
+
+	p.Monitor.Infof("Registration activity for participant '%s' completed successfully", registrationData.DID)
+	// create holder in ApiClient
+	return api.ActivityResult{Result: api.ActivityResultComplete}
+}
---
+++ agent/registration/activity/activity_test.go
@@ -0,0 +1,153 @@
+//  Copyright (c) 2025 Metaform Systems, Inc
+//
+//  This program and the accompanying materials are made available under the
+//  terms of the Apache License, Version 2.0 which is available at
+//  https://www.apache.org/licenses/LICENSE-2.0
+//
+//  SPDX-License-Identifier: Apache-2.0
+//
+//  Contributors:
+//       Metaform Systems, Inc. - initial API and implementation
+//
+
+package activity
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/metaform/connector-fabric-manager/common/model"
+	"github.com/metaform/connector-fabric-manager/common/system"
+	"github.com/metaform/connector-fabric-manager/pmanager/api"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRegistrationActivityProcessor_MinimalValidData(t *testing.T) {
+	issuerService := MockIssuerService{}
+	processor := NewProcessor(&Config{
+		LogMonitor:    system.NoopMonitor{},
+		IssuerService: &issuerService,
+	})
+
+	ctx := context.Background()
+	outputData := make(map[string]any)
+
+	activity := api.Activity{
+		ID:   "test-activity",
+		Type: "edcv",
+	}
+
+	processingData := map[string]any{
+		model.ParticipantIdentifier: "did:web:someparticipant",
+	}
+
+	activityContext := api.NewActivityContext(ctx, "orch-123", activity, processingData, outputData)
+
+	result := processor.Process(activityContext)
+
+	assert.Equal(t, api.ActivityResultType(api.ActivityResultComplete), result.Result)
+	assert.NoError(t, result.Error)
+	assert.Equal(t, "did:web:someparticipant", issuerService.recorded.did)
+	assert.Equal(t, "did:web:someparticipant", issuerService.recorded.name)
+}
+
+func TestRegistrationActivityProcessor_FullValidData(t *testing.T) {
+	issuerService := MockIssuerService{}
+	processor := NewProcessor(&Config{
+		LogMonitor:    system.NoopMonitor{},
+		IssuerService: &issuerService,
+	})
+
+	ctx := context.Background()
+	outputData := make(map[string]any)
+
+	activity := api.Activity{
+		ID:   "test-activity",
+		Type: "edcv",
+	}
+
+	processingData := map[string]any{
+		model.ParticipantIdentifier:  "did:web:someparticipant",
+		"cfm.participant.holdername": "some holder",
+	}
+
+	activityContext := api.NewActivityContext(ctx, "orch-123", activity, processingData, outputData)
+
+	result := processor.Process(activityContext)
+
+	assert.Equal(t, api.ActivityResultType(api.ActivityResultComplete), result.Result)
+	assert.NoError(t, result.Error)
+	assert.Equal(t, "did:web:someparticipant", issuerService.recorded.did)
+	assert.Equal(t, "some holder", issuerService.recorded.name)
+}
+
+func TestRegistrationActivityProcessor_InvalidData(t *testing.T) {
+	issuerService := MockIssuerService{}
+	processor := NewProcessor(&Config{
+		LogMonitor:    system.NoopMonitor{},
+		IssuerService: &issuerService,
+	})
+
+	ctx := context.Background()
+	outputData := make(map[string]any)
+
+	activity := api.Activity{
+		ID:   "test-activity",
+		Type: "edcv",
+	}
+
+	processingData := map[string]any{}
+
+	activityContext := api.NewActivityContext(ctx, "orch-123", activity, processingData, outputData)
+
+	result := processor.Process(activityContext)
+
+	assert.Equal(t, api.ActivityResultType(api.ActivityResultFatalError), result.Result)
+	assert.Error(t, result.Error)
+}
+
+func TestRegistrationActivityProcessor_IssuerServiceFails(t *testing.T) {
+	issuerService := MockIssuerService{expectedError: fmt.Errorf("some error")}
+	processor := NewProcessor(&Config{
+		LogMonitor:    system.NoopMonitor{},
+		IssuerService: &issuerService,
+	})
+
+	ctx := context.Background()
+	outputData := make(map[string]any)
+
+	activity := api.Activity{
+		ID:   "test-activity",
+		Type: "edcv",
+	}
+
+	processingData := map[string]any{
+		model.ParticipantIdentifier: "did:web:someparticipant",
+	}
+
+	activityContext := api.NewActivityContext(ctx, "orch-123", activity, processingData, outputData)
+
+	result := processor.Process(activityContext)
+
+	assert.Equal(t, api.ActivityResultType(api.ActivityResultFatalError), result.Result)
+	assert.ErrorContains(t, result.Error, "some error")
+}
+
+type regData struct {
+	did      string
+	holderID string
+	name     string
+}
+
+type MockIssuerService struct {
+	expectedError error
+	recorded      regData
+}
+
+func (m *MockIssuerService) CreateHolder(did string, holderID string, name string) error {
+	m.recorded.did = did
+	m.recorded.holderID = holderID
+	m.recorded.name = name
+	return m.expectedError
+}
