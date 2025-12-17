9ba2eedc10029bd6ac3d03df0f93aa32788b2279
Jim Marino <jim.marino@gmail.com>
2025-12-17 19:12:40 +0100

    Remove obsolete Terraform files

--- deployments/terraform/modules/pmanager/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "pmanager_service_name" {
-  description = "The name of the provision manager service"
-  value       = var.pmanager_service
-}
-
-output "pmanager_port" {
-  description = "The port that the provision manager HTTP server listens on"
-  value       = var.pmanager_port
-}
--- deployments/terraform/modules/tmanager/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "tmanager_service_name" {
-  description = "The name of the tenant manager service"
-  value       = var.tmanager_service
-}
-
-output "tmanager_port" {
-  description = "The port that the tenant manager HTTP server listens on"
-  value       = var.tmanager_port
-}
